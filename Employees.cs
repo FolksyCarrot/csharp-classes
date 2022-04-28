977a8356ac757461c9cfc93baafc75fbcc747d4a
Keith Parks <keithparks20@gmail.com>
2022-04-28 11:44:15 -0500

    update

--- Employees.cs
+++ Employees.cs
@@ -6,6 +6,8 @@ namespace Classes {
 
           public string name {get; set;} 
           public string position {get; set;}
+          public string title {get; set;}
+          public DateTime startDate {get; set;}
 
           public Employees(string fullName, string role) {
               name = fullName;
